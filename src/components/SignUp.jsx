810a8aa0651db37d48d2b7a532c14d1fe0cd1b12
IqraMalik99 <im4356927@gmail.com>
2025-06-15 20:07:35 +0500

    Update SignUp.jsx

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -21,8 +21,7 @@ function SignUp() {
         try {
             console.log(data);
             const response = await axios.post('https://realestae-backened-production.up.railway.app/user/sign-up', data, { withCredentials: true });
-            console.log('Response from server:', response.data.data.data.username);
-              console.log('Response from server:', response.data.data.username);
+              console.log('Response from server:', response.data.data);
             reset();
             let userdata = {
                 username: response.data.data.user.username,
